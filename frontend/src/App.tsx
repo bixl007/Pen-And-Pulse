aa88450b5ea19da6c2cc4a29d25407b104006d3e
Bishal Baira <bishalbaira432@gmail.com>
2024-07-26 21:58:05 +0530

    Feat: fix route issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
